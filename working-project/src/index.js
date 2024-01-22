2c83262aa9c1493759979adb91fe6013b1aa955f
koketso-magoro <koketso.magoro06@gmail.com>
2024-01-22 15:16:04 +0200

    fixing a bug in my search input

--- working-project/src/index.js
+++ working-project/src/index.js
@@ -1,19 +1,20 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
 }
 
 function search(event) {
   event.preventDefault();
-  let cityElement = document.querySelector("#current-city");
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
+
   let apiKey = "00ee90f9ftdb3baef0aad3543o2ac43c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
-  cityElement.innerHTML = city;
 }
 
 function formatDate(date) {
